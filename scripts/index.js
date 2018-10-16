1318a40041b6e6c4150f69e6186d9780f6ef1d81
Ian Hundere <ianfundere@gmail.com>
2018-10-16 14:39:08 -0400

    adds lines / numbers

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,20 @@ function getAdvice() {
     fetch('http://api.adviceslip.com/advice')
         .then(convertToJson)
         .then(extractAdviceText)
-        .then(drawAdvice)
+        .then(drawAdvice);
 }
-
 function convertToJson(r) {
     return r.json();
 }
-
 function extractAdviceText(adviceObj) {
     return adviceObj.slip.advice;
 }
 function drawAdvice(adviceText) {
     const newAdvice = document.createElement('li');
+    const newLine = document.createElement('hr');
     newAdvice.textContent = adviceText;
     outputElement.appendChild(newAdvice);
+    outputElement.appendChild(newLine);
 }
 function main() {
     triggerElement.addEventListener('click', getAdvice);
