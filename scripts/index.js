fd1ec2355c5b55f5b9c040227eabb480f2fa1a96
Ian Hundere <ianfundere@gmail.com>
2018-10-17 16:31:07 -0400

    adds images

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
-const triggerElement = document.querySelector('[ data-trigger]');
-const outputElement = document.querySelector('[ data-output]');
+const triggerElement = document.querySelector('[data-trigger]');
+const outputElement = document.querySelector('[data-output]');
+
 function getAdvice() {
     fetch('http://api.adviceslip.com/advice')
         .then(convertToJson)
@@ -7,6 +8,7 @@ function getAdvice() {
         .then(drawAdvice);
 }
 function convertToJson(r) {
+    // console.log(r);
     return r.json();
 }
 function extractAdviceText(adviceObj) {
@@ -19,7 +21,28 @@ function drawAdvice(adviceText) {
     outputElement.appendChild(newAdvice);
     outputElement.appendChild(newLine);
 }
+
+const triggerElementImg = document.querySelector('[data-triggerImg]');
+const outputElementImg = document.querySelector('[data-outputImg]');
+
+function getHero() {
+    fetch('https://pixabay.com/api/?key=10424289-561cb90cf88816a7238a655ad&q=magic&image_type=photo')
+      .then(convertToJson)
+      .then(extractHeroImage)
+      .then(drawHero);
+}
+function extractHeroImage(heroObj) {
+    random = Math.floor(Math.random() * 30) + 1;  
+    return heroObj.hits[random].userImageURL;
+}
+function drawHero(heroImg) {
+    // console.log(heroImg);
+  const newHero = document.createElement('img');
+  newHero.src = heroImg;
+  outputElementImg.appendChild(newHero);
+}
 function main() {
-    triggerElement.addEventListener('click', getAdvice);
+  triggerElement.addEventListener("click", getAdvice);
+  triggerElementImg.addEventListener("click", getHero);
 }
 main();
