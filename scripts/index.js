524cf2957684b33271788b834c278c47864fdce1
Ian Hundere <ianfundere@gmail.com>
2018-10-18 07:20:18 -0400

    adds cached promises

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,36 @@ const triggerElement = document.querySelector('[data-trigger]');
 const outputElement = document.querySelector('[data-output]');
 
 function getAdvice() {
-    fetch('http://api.adviceslip.com/advice')
-        .then(convertToJson)
-        .then(extractAdviceText)
-        .then(drawAdvice);
+    fetch("http://api.adviceslip.com/advice")
+      .then(convertToJson)
+      .then(cacheAdvice)
+      .then(extractAdviceText)
+      .catch(showCachedAdvice)
+      .then(drawAdvice);
 }
+
 function convertToJson(r) {
     // console.log(r);
     return r.json();
 }
+
+function cacheAdvice(adviceObj) {
+    if (adviceObj.slip.advice) {
+      console.log("Cache advice to localStorage");
+        localStorage.setItem("advice", adviceObj.slip.advice);
+    }
+    return adviceObj;
+}
+
+function showCachedAdvice(err) {
+  console.log(err);
+  return localStorage.getItem("advice");
+}
+
 function extractAdviceText(adviceObj) {
     return adviceObj.slip.advice;
 }
+
 function drawAdvice(adviceText) {
     const newAdvice = document.createElement('li');
     const newLine = document.createElement('hr');
@@ -31,18 +49,22 @@ function getHero() {
       .then(extractHeroImage)
       .then(drawHero);
 }
+
 function extractHeroImage(heroObj) {
     random = Math.floor(Math.random() * 30) + 1;  
     return heroObj.hits[random].userImageURL;
 }
+
 function drawHero(heroImg) {
     // console.log(heroImg);
   const newHero = document.createElement('img');
   newHero.src = heroImg;
   outputElementImg.appendChild(newHero);
 }
+
 function main() {
   triggerElement.addEventListener("click", getAdvice);
   triggerElementImg.addEventListener("click", getHero);
 }
+
 main();
